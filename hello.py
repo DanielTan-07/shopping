806656bcce238f964ee061a1491dfedc0b4e9338
Daniel Tan <tempdan@yahoo.com>
2024-09-01 15:08:11 +0800

    remove the 2 additional file

--- hello.py
+++
@@ -1,6 +0,0 @@
-import pkg_resources
-try:
-    version = pkg_resources.get_distribution("pyramid").version
-    print("Pyramid version:", version)
-except pkg_resources.DistributionNotFound:
-    print("Pyramid is not installed.")
